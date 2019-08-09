a495a19440349f98ba88b85b7d01134e6ee93ace
K Hong <albtrossfacefall@gmail.com>
2019-08-09 01:38:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,8 +40,10 @@ def using_flatten(array)
   array.flatten
 end 
 
-def using_delete_at(array,integer)
+def using_delete(array, string)
   array.delete(string)
 end
 
-def
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
